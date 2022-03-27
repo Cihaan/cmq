a3491d006afe95238fa137ce76cad67a1dff9025
Cihaan <guide7169@gmail.com>
2022-03-27 15:40:43 +0200

    routing

--- src/App.jsx
+++ src/App.jsx
@@ -5,30 +5,18 @@ import Landing from "./components/Landing";
 import Home from "./components/Home";
 import Food from "./components/Food";
 
-function App() {
+export default function App() {
   return (
     <div className="App">
       <div className="min-h-screen bg-[#0F172A] p-5">
         <Routes>
           <Route path="/" element={<Landing />} />
+          <Route path="/landing" element={<Landing />} />
           <Route path="home/" element={<Home />} />
           <Route path="food/" element={<Food />} />
+          <Route path="/*" element={<Landing />} />
         </Routes>
       </div>
     </div>
   );
-}
-
-export default App;
-
-{
-  /* <Router>
-          <NavBar />
-
-          <Route path="/" exact component={Landing} />
-          <Route path="/home" exact component={Home} />
-          <Route path="/food" exact component={Food} />
-          <Route path="/add" exact component={Add} />
-          <Route path="/profil" exact component={Profil} />
-        </Router> */
 }
