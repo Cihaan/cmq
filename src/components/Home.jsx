fbe9ea3cdc49542f95e9a006988760b36ff3e35a
Cihaan <guide7169@gmail.com>
2022-03-27 21:09:01 +0200

    start home page

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -5,8 +5,13 @@ export default function Home() {
     
     return (
         <div>
+            <div className='h-screen'>
+                <button className='w-full h-10 text-[#37B9F3] text-xl font-[Ubuntu] rounded border-2'>Ca mange quoi</button>
+                <div className=''>
+
+                </div>
+            </div>
             <NavBar />
-            
         </div>
     )
 }
